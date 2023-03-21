06d602a4cca2faebd3a87b57904e275e3ebea384
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-03-21 09:50:40 -0300

    borrar las acciones

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,8 @@
-import { ADD_CHARACTER, DELETE_CHARACTER, FILTER_CARDS, ORDER_CARDS } from '../redux/actions.js'
+import { ADD_CHARACTER } from '../redux/actions.js'
 
 
 const initialState = {
-    myFavorites: [],
+    
     allCharacters: [],
 };
 
@@ -11,36 +11,7 @@ function reducer(state = initialState, action) {
         case ADD_CHARACTER:
             return {
                 ...state,
-                myFavorites: [...state.allCharacters, action.payload],
                 allCharacters: [...state.allCharacters, action.payload],
-            };
-        case DELETE_CHARACTER:
-            return {
-                ...state,
-                myFavorites: state.myFavorites.filter((char) => {
-                    return char.id !== action.payload;
-                }),
-            };
-        case FILTER_CARDS:
-            const fileteredCharacters = state.allCharacters.filter((char) => {
-                return char.gender === action.payload;
-            });
-            return {
-                ...state,
-                myFavorites: fileteredCharacters,
-            }
-        case ORDER_CARDS:
-            const sortedCharacters = [...state.allCharacters].sort((a, b) => {
-                if (action.payload === "Ascendete") {
-                    return a.id - b.id;
-                } else if (action.payload === "Descendiente") {
-                    return b.id - a.id;
-                }
-                return 0;
-            });
-            return {
-                ...state,
-                myFavorites: sortedCharacters,
             };
         default:
             return { ...state };

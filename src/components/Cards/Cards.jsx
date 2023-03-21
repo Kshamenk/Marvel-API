65a3a3662186cadf7ee9b6dfb8546622c8c9bd40
Carlos Martin Juncos <mjuncos@soyhenry.com>
2023-03-21 19:17:33 -0300

    estilos

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,6 @@
-import  {useState, useEffect} from "react";
+import  {useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {getCharacter} from '../../redux/actions.js'
-<<<<<<< HEAD
-
-=======
-import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
->>>>>>> 5145151951e218516d950bf6106387e878dc97e2
 import styles from "./Cards.module.css";
 
 export default function Cards() {

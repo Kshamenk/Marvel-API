2e39383c38550ae07f90fe071c936c5db9ca1459
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-03-22 11:53:14 -0300

    115322032023

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,28 +1,42 @@
-import  {useEffect} from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {getCharacter} from '../../redux/actions.js'
+import { getCharacter } from '../../redux/actions.js'
 import styles from "./Cards.module.css";
+import Card from "../card/Card.jsx";
 
 export default function Cards() {
   const characters = useSelector((state) => state.allCharacters);
 
   const dispatch = useDispatch();
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(getCharacter())
   }, [dispatch])
 
   return (
-    <div className={styles.Cards} >
+    <div>
       <h1>Cards</h1>
-      {
-        characters.map(char => {
-          return (
-            <p key={char.id} >{char.name}</p>
-          )
-        })
-      }
+      <div className={styles.Cards} >
+        {
+          characters.map(char => {
+            const url = char.thumbnail.path
+            const ext = char.thumbnail.extension
 
+            return (
+              <Card
+                id={char.id}
+                name={char.name}
+                img={`${url}.${ext}`}
+                description={char.description}
+
+              />
+
+            )
+          })
+
+        }
+
+      </div>
     </div>
   );
 }

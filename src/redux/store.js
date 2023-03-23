94a9722544cc9251e7c613dd702b758e7adf3c04
Carlos Martin Juncos <mjuncos@soyhenry.com>
2023-03-23 12:34:32 -0300

    230320231234

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { legacy_createStore as createStore, applyMiddleware, compose } from "red
 import reducer from "../redux/reducer.js";
 import  thunkMiddleware  from "redux-thunk";
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose; 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
 

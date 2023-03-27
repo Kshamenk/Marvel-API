3f6993c98e1cd17ca331f02c81415f7a24c837e1
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-03-27 10:38:30 -0300

    103827032023

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from '../card/Card.module.css';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { addFavorite, deleteFavorites } from '../../redux/actions.js';
+import { addFavorite } from '../../redux/actions.js';
 
 export default function Card(props) {
   const { name, img, detailId } = props;
@@ -13,30 +13,25 @@ export default function Card(props) {
     dispatch(addFavorite(props));
   }
 
-  function handleDelete() {
-    dispatch(deleteFavorites(detailId))
-  }
-
   function handleFavorite() {
     if (isFav) {
       setIsFav(false)
-      handleDelete()
     } else {
       setIsFav(true)
+
       handleAdd()
     }
   }
 
   return (
     <div className={styles.card}>
-      <button onClick={handleDelete} >X</button>
       {
-   isFav ? (
-      <button onClick={handleFavorite}>❤️</button>
-   ) : (
-      <button onClick={handleFavorite}>🤍</button>
-   )
-}
+        isFav ? (
+          <button onClick={handleFavorite}>❤️</button>
+        ) : (
+          <button onClick={handleFavorite}>🤍</button>
+        )
+      }
       <Link to={`/detail/${detailId}`}>
         <h3>{name}</h3>
       </Link>

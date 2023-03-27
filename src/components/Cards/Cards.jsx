8bec3cb386a70e0e681d3b7ca9abfc1612580089
Kshamenk Benitez <kbelpirata007@gmail.com>
2023-03-27 16:26:01 -0300

    conflicto resuelto

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getCharacters } from '../../redux/actions.js'
+
+import { getCharacter } from "../../redux/actions.js";
 import styles from "./Cards.module.css";
 import Card from "../card/Card.jsx";
-import SearchBar from '../searchbar/SearchBar.jsx';
+import SearchBar from "../searchbar/SearchBar.jsx";
 
 export default function Cards() {
   const characters = useSelector((state) => state.allCharacters);
@@ -11,30 +12,28 @@ export default function Cards() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getCharacters())
-  }, [dispatch])
+    dispatch(getCharacter());
+  }, [dispatch]);
 
   return (
     <div>
       <SearchBar />
 
-      <div className={styles.Cards} >
-        {
-          characters.map(char => {
-            const url = char.thumbnail.path
-            const ext = char.thumbnail.extension
+      <div className={styles.Cards}>
+        {characters.map((char) => {
+          const url = char.thumbnail.path;
+          const ext = char.thumbnail.extension;
 
-            return (
-              <Card
-                id={char.id}
-                name={char.name}
-                img={`${url}.${ext}`}
-                description={char.description}
-                detailId={char.id}
-              />
-            )
-          })
-        }
+          return (
+            <Card
+              id={char.id}
+              name={char.name}
+              img={`${url}.${ext}`}
+              description={char.description}
+              detailId={char.id}
+            />
+          );
+        })}
       </div>
     </div>
   );

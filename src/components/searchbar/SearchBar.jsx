045f0cbc81b94be64ba3f14985bd94c4d5bbf05c
Kshamenk Benitez <kbelpirata007@gmail.com>
2023-03-30 17:28:22 -0300

    172830302023

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,31 +1,50 @@
-import React, {useState} from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-import styles from "../searchbar/SearchBar.module.css";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import styles from "./SearchBar.module.css";
 import { getCharacter } from "../../redux/actions";
 import { useDispatch } from "react-redux";
 
 export default function SearchBar() {
-const [input, setInput] = useState("");
-const dispatch = useDispatch();
+  const [input, setInput] = useState("");
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth < 600);
+    }
 
-function handleCharacter() {
-  dispatch(getCharacter(input))
-}
+    window.addEventListener("resize", handleResize);
+    handleResize();
 
-const handleInputChange = (event) => {
-  var value = event.target.value;
-  setInput(value);
-}
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
+  function handleCharacter() {
+    dispatch(getCharacter(input));
+  }
 
+  const handleInputChange = (event) => {
+    var value = event.target.value;
+    setInput(value);
+  };
 
   return (
     <div className={styles.container}>
-      <input type="search" onChange={handleInputChange} placeholder= 'Buscar...'/>
-      <button className={styles.search} onClick={handleCharacter} ><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-      
+      <input
+        type="search"
+        onChange={handleInputChange}
+        placeholder="Buscar..."
+      />
+      {isSmallScreen ? (
+        <button className={styles.search} onClick={handleCharacter}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
+      ) : null}
+      <button className={`${styles.search} ${styles["search-large"]}`} onClick={handleCharacter}>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </button>
     </div>
   );
 }

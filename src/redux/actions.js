817eb86b2a4c2903121062320dda825ae8313fd7
Carlos Martin Juncos <mjuncos@soyhenry.com>
2023-03-31 12:07:37 -0300

    agregar personajes

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -8,7 +8,7 @@ export const GET_CHARACTER = "GET_CHARACTER"
 export const getCharacters = () => {
     return function (dispatch){
         return (
-            fetch('https://gateway.marvel.com/v1/public/characters?ts=martin&apikey=c196baefce684067e1d3d532d02b59d2&hash=5fac97994a159f64c87251906fd8a7fc')
+            fetch('https://gateway.marvel.com/v1/public/characters?limit=80&ts=martin&apikey=c196baefce684067e1d3d532d02b59d2&hash=5fac97994a159f64c87251906fd8a7fc')
             .then(res => res.json())
             .then(data => dispatch({type: GET_CHARACTERS, payload: data.data.results}))
         )
